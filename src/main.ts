62661e5a0f8927f618d69d95e15b1d56f28a790f
Billy Zoellers <billy.zoellers@mac.com>
2022-04-19 15:13:57 -0400

    integrate Cisco xapi

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,125 @@
-// https://developer.scrypted.app/#getting-started
-// package.json contains the metadata (name, interfaces) about this device
-// under the "scrypted" key.
-import axios from 'axios';
-import { HttpRequestHandler, HumiditySensor, ScryptedDeviceBase, TemperatureUnit, Thermometer, HttpRequest, HttpResponse, BinarySensor, OccupancySensor, Settings, Setting, SettingValue } from '@scrypted/sdk';
+/*
+ *  Scrypted plugin for Cisco Webex Device
+ *
+ */
+import { HttpRequestHandler, HumiditySensor, ScryptedDeviceBase, TemperatureUnit, Thermometer, HttpRequest, HttpResponse, BinarySensor, OccupancySensor, Settings, Setting, SettingValue, Refresh, DeviceInformation } from '@scrypted/sdk';
 import sdk from '@scrypted/sdk';
 const { log, endpointManager } = sdk;
+const jsxapi = require('jsxapi');
 
-class WebexDevice extends ScryptedDeviceBase implements Thermometer, HumiditySensor, HttpRequestHandler, BinarySensor, OccupancySensor, Settings {
+const FEEDBACK_SLOT = 2;
+
+class WebexDevice extends ScryptedDeviceBase implements Thermometer, HumiditySensor, HttpRequestHandler, BinarySensor, OccupancySensor, Settings, Refresh {    
+    xapi: typeof jsxapi
+    httpEndpoint: string
+    
     constructor() {
         super();
+        this.temperatureUnit = TemperatureUnit.C;
+        this.xapi = undefined;
 
         endpointManager.getInsecurePublicLocalEndpoint().then(endpoint => {
-            this.console.log(endpoint);
-
+            this.httpEndpoint = endpoint.replace("127.0.0.1", this.storage.getItem('scrypted_server_ip'));
+            this.connectXAPI();
         })
 
     }
-    async getSettings(): Promise<Setting[]> {
-        throw new Error('Method not implemented.');
+
+    connectXAPI() {
+        jsxapi
+        .connect(`wss://${this.storage.getItem('device_ip')}`, {
+            username: this.storage.getItem('device_username'),
+            password: this.storage.getItem('device_password'),
+        })
+        .on('error', this.console.error)
+        .on('ready', async (xapi) => {
+            this.xapi = xapi;
+            this.console.log(`Connected to xapi of ${this.storage.getItem('device_ip')}`)
+
+            // Register HttpFeedback
+            this.xapi.Command.HttpFeedback.Register({
+                Expression: [
+                    "/Event/CallSuccessful",
+                    "/Event/CallDisconnect",
+                    "/Status/RoomAnalytics/PeoplePresence",
+                    "/Status/RoomAnalytics/PeopleCount",
+                ],
+                FeedbackSlot: FEEDBACK_SLOT,
+                Format: "json",
+                ServerUrl: this.httpEndpoint
+            });
+
+            this.xapi.Status.SystemUnit
+                .get()
+                .then((SystemUnit) => {
+                    this.info = {
+                        manufacturer: SystemUnit.ProductType,
+                        model: SystemUnit.ProductId,
+                        serialNumber: SystemUnit.Hardware.Module.SerialNumber,
+                        firmware: (SystemUnit.Software.Version).replace("ce", ""),
+                    }
+                })
+
+        });
     }
-    
+
+    disconnectXAPI() {
+        this.xapi.Command.HttpFeedback.Deregister(
+            { FeedbackSlot: FEEDBACK_SLOT });
+        this.xapi.close();
+        this.xapi = undefined;
+    }
+
+    async getRefreshFrequency(): Promise<number> {
+        return 60;
+    }
+
+    async refresh(refreshInterface: string, userInitiated: boolean): Promise<void> {
+        this.console.log(`${refreshInterface} requested refresh: ${new Date()}`);
+
+        this.xapi.Status.RoomAnalytics
+            .get()
+            .then((RoomAnalytics) => {
+                this.console.log(`Temperature ${RoomAnalytics.AmbientTemperature}, Humidity ${RoomAnalytics.RelativeHumidity}, PeoplePresence ${RoomAnalytics.PeoplePresence}, PeopleCount ${RoomAnalytics.PeopleCount}`);
+                
+                this.temperature = RoomAnalytics.AmbientTemperature;
+                this.humidity = RoomAnalytics.RelativeHumidity;
+                this.occupied = (RoomAnalytics.PeoplePresence === "Yes");
+            })
+
+    }
+
+    async getSettings(): Promise<Setting[]> {
+        return [
+            {
+                title: "Device IP",
+                key: "device_ip",
+                value: this.storage.getItem("device_ip"),
+                description: "IP of Webex Device"
+            },
+            {
+                title: "Username",
+                key: "device_username",
+                value: this.storage.getItem("device_username"),
+                description: "Username on Webex device"
+            },
+            {
+                title: "Password",
+                key: "device_password",
+                value: this.storage.getItem("device_password"),
+                description: "Password on Webex device"
+            },
+            {
+                title: "Scrypted Server IP",
+                key: "scrypted_server_ip",
+                value: this.storage.getItem("scrypted_server_ip"),
+                description: "IP of the Scrypted server"
+            }
+        ]
+    }
+
     async putSetting(key: string, value: SettingValue): Promise<void> {
-        throw new Error('Method not implemented.');
+        this.storage.setItem(key, value.toString());
     }
 
     async onRequest(request: HttpRequest, response: HttpResponse) {
@@ -30,28 +128,17 @@ class WebexDevice extends ScryptedDeviceBase implements Thermometer, HumiditySen
         const json = JSON.parse(request.body)
         this.console.log(json)
 
-        const humidity = json.Status?.RoomAnalytics?.RelativeHumidity
-        if (humidity) {
-            this.console.log(`Humidity ${humidity}`)
-            this.humidity = Number(humidity);
-        }
-
-        const temperature = json.Status?.RoomAnalytics?.AmbientTemperature
-        if (temperature) {
-            this.console.log(`Temperature ${temperature}`)
-            this.temperature = Number(temperature);
-        }
-
-        
         const callConnect = json.Event?.CallSuccessful
         if (callConnect) {
-            this.console.log(`Call connected: ${callConnect}`)
+            this.console.log('Call connected')
+            this.console.log(callConnect)
             this.binaryState = true;
         }
 
         const callDisconnect = json.Event?.CallDisconnect
         if (callDisconnect) {
-            this.console.log(`Call disconnected ${callDisconnect}`)
+            this.console.log('Call disconnected')
+            this.console.log(callDisconnect)
             this.binaryState = false;
         }
 
